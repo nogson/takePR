1b5eebf95eb15e06c2dbde26af4bb856e1801f14
nogson <nogson@users.noreply.github.com>
2024-07-17 09:54:35 +0000

    feat: update variables

---
+++ ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,15 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 17 Jul 2024 09:54:34 GMT
+
+
+import UIKit
+
+public enum  {
+    public static let colorColorDark = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorColorLight = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+}
